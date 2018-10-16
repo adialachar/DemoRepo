ed25625bf6be86b668d3358981909ab8f7e08188
Ted Kim <ted.s.kim1004@gmail.com>
2018-10-15 19:40:14 -0700

    hi

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 
 int main(){
+	string yeet = "yeetus deletus";
 
 	cout << "Hello Mars" << endl;
+	cout << "ya yeet" << endl;
+	cout << "yeetus deletus" << endl;
+
+	for (unsigned i = 0; i < yeet.size(); ++i) { 
+		cout << yeet.at(i) << endl;
+		cout << endl;
+		if (yeet.at(i) == 'e') {
+			cout << "POOOOOOOOOOOP" << endl;
+			cout << "yayeet" << endl;
+		}
+		else {
+			cout << "PEEEEEEEEE" << endl;
+			cout << "yayeet" << endl;
+		}
+	}
 
 
 
